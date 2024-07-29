d123288a386ce350e78079c8e233799d30baeebb
janholub <fxxholub@icloud.com>
2024-07-29 09:32:30 +0200

    session get by id specification

---
+++ src/Leuze_AGV_Handling_Service.Core/SessionAggregate/Specifications/SessionByIdSpec.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+
+namespace Leuze_AGV_Handling_Service.Core.SessionAggregate.Specifications;
+
+public class SessionByIdSpec : Specification<Session>
+{
+  public SessionByIdSpec(int sessionId)
+  {
+    Query
+        .Where(sesh => sesh.Id == sessionId);
+  }
+}

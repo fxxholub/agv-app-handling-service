cc25f2d810bebfd9300077e5eba3e9c625279746
janholub <fxxholub@icloud.com>
2024-09-11 13:53:25 +0200

    moved unused process scripts to a new folder

---
+++ ProcessScripts/test1/Common/1_ros_pub_map.sh
@@ -0,0 +1,3 @@
+source /opt/ros/humble/setup.bash
+
+ros2 topic pub /map std_msgs/msg/String "data: 'something'" --rate 1
---
+++ ProcessScriptsBin/host2/Common/6_scan_merge.sh
@@ -0,0 +1,6 @@
+﻿cd ros2_ws
+
+source /opt/ros/humble/setup.bash
+source install/local_setup.bash
+
+ros2 launch ros2_laser_scan_merger merge_2_scan.launch.py

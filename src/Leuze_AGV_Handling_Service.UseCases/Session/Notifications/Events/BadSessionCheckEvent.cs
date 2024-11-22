85e2b42b7bfba823ec65d5b2a1342fef9fc8b024
janholub <fxxholub@icloud.com>
2024-11-22 08:39:45 +0100

    session use case event as record

--- src/Leuze_AGV_Handling_Service.UseCases/Session/Notifications/Events/BadSessionCheckEvent.cs
+++ src/Leuze_AGV_Handling_Service.UseCases/Session/Notifications/Events/BadSessionCheckEvent.cs
@@ -5,12 +5,4 @@ namespace Leuze_AGV_Handling_Service.UseCases.Session.Notifications.Events;
 /// <summary>
 /// Notification for a bad session check.
 /// </summary>
-public class BadSessionCheckEvent : INotification
-{
-    public int SessionId { get; }
-
-    public BadSessionCheckEvent(int sessionId)
-    {
-        SessionId = sessionId;
-    }
-}
+public record BadSessionCheckEvent(int SessionId) : INotification;

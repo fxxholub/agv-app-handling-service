ff9e354fd5974f20c5f2fcfa032048f053b3b8e5
janholub <fxxholub@icloud.com>
2025-01-28 11:58:24 +0100

    manual mode agv_mode sent with joy cmd_vel topic

--- src/Leuze_AGV_Handling_Service.Infrastructure.Ros2/Nodes/ManualPublisher.cs
+++ src/Leuze_AGV_Handling_Service.Infrastructure.Ros2/Nodes/ManualPublisher.cs
@@ -11,6 +11,7 @@ public class ManualPublisher : IManualPublisher
 {
     private readonly ILogger<ManualPublisher> _logger;
 
+    private readonly IRclPublisher<Ros2CommonMessages.Std.String> _agvModePublisher;
     private readonly IRclPublisher<Ros2CommonMessages.Geometry.Twist> _joyPublisher;
     public ManualPublisher(IServiceProvider serviceProvider, ILogger<ManualPublisher> logger)
     {
@@ -21,17 +22,30 @@ public class ManualPublisher : IManualPublisher
         var node = context.CreateNode("handling_service_manual_pub");
 
         // called joy for teleop_twist_joy package purposes
+        _agvModePublisher = node.CreatePublisher<Ros2CommonMessages.Std.String>("/AgvMode");
         _joyPublisher = node.CreatePublisher<Ros2CommonMessages.Geometry.Twist>("/cmd_vel");
     }
-
+    
+    public async Task PublishAgvModeTopic()
+    {
+        var msg = new Ros2CommonMessages.Std.String
+        {
+            Data = "manual",
+        };
+        
+        await _agvModePublisher.PublishAsync(msg);
+    }
+    
     public async Task PublishJoyTopic(float x, float y, float w)
     {
         var msg = new Ros2CommonMessages.Geometry.Twist
         {
-            Linear = new Ros2CommonMessages.Geometry.Vector3(-x/100, -y/100, 0),
-            Angular = new Ros2CommonMessages.Geometry.Vector3(0, 0, -w/100)
+            // velocity limit for axis 0.1 m/s
+            Linear = new Ros2CommonMessages.Geometry.Vector3(-x/1000, -y/1000, 0),
+            Angular = new Ros2CommonMessages.Geometry.Vector3(0, 0, -w/1000)
         };
-        
+
+        await PublishAgvModeTopic();
         await _joyPublisher.PublishAsync(msg);
     }
 }
